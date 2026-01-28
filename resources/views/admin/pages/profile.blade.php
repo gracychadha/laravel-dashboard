910f93d62c3593d18474ec36202f36f0bf8176f3
gracychadha <chadhagracy8@gmail.com>
2026-01-28 11:05:02 +0530

    gitignore update

--- resources/views/admin/pages/profile.blade.php
+++ resources/views/admin/pages/profile.blade.php
@@ -22,7 +22,7 @@
                         text: 'Profile updated successfully!',
                         timer: 4000,
                         timerProgressBar: true,
-                    });
+                        });
                 </script>
             @endif
 
